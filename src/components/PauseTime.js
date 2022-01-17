06ad1a975d6e2499384b96f746c94925d11b4209
Mayank Budhiraja <mayankbudhiraja3@gmail.com>
2022-01-16 21:28:24 -0800

    changeFLOW: home theme fixed

--- src/components/PauseTime.js
+++ src/components/PauseTime.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import RadioGroup from 'react-native-radio-buttons-group';
+import colors from '../constants/colors';
 
 const PauseTime = ({onClick, buttonsData}) => {
+  //to-do change radio button colour
   return (
     <View style={styles.itemContainer}>
       <Text style={styles.textContainer}>Pause Time</Text>
@@ -26,6 +28,7 @@ const styles = StyleSheet.create({
   textContainer: {
     top: 5,
     fontSize: 18,
+    color: colors.app_color_primary
   },
   itemContainer: {
     top: 20,
